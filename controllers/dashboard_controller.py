d29bf3974f585178f753a56da46fb84d845a0d33
Igor Cieply <igoracieply@outlook.com>
2023-10-18 17:16:43 -0500

    Corrected balance on dashboard to be dollars and cents instead of a float

--- controllers/dashboard_controller.py
+++ controllers/dashboard_controller.py
@@ -6,5 +6,6 @@ from models.dashboard_model import get_user_balance
 @app.route("/dashboard", methods=["POST", "GET"])
 def dashboard_page():
     id = request.cookies.get("id")
-    balance = get_user_balance(int(id or 0))
+    # Arcane code that formats the float value as dollars and cents
+    balance = "${:,.2f}".format(get_user_balance(int(id or 0)))
     return render_template("dashboard.html", id=id, balance=balance)
