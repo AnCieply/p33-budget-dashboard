2830df0574112af9c14914eacf784bfd024e0d12
Igor Cieply <igoracieply@outlook.com>
2023-10-23 16:05:02 -0500

    Added connection to AzureSQL

--- models/db_models.py
+++ models/db_models.py
@@ -3,8 +3,8 @@ from init import db
 class Account(db.Model):
     id = db.Column(db.Integer, default=0)
     username = db.Column(db.String(100), nullable=False, primary_key=True)
-    password = db.Column(db.BLOB, nullable=False)
-    salt = db.Column(db.BLOB, nullable=False)
+    password = db.Column(db.LargeBinary, nullable=False)
+    salt = db.Column(db.LargeBinary, nullable=False)
 
 class UserData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
