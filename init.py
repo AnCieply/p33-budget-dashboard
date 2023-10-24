d2f0bad906119936160d5c90fe5abb45a6cd979b
Igor Cieply <igoracieply@outlook.com>
2023-10-24 14:44:45 -0500

    Corrected database uri

--- init.py
+++ init.py
@@ -4,6 +4,6 @@ from os import urandom
 
 # App and database initialization
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql://p33shadowadmin:LongWill2@p33.database.windows.net:1433/P33Data?driver=ODBC+Driver+18+for+SQL+Server"
 app.secret_key = urandom(24)
 db = SQLAlchemy(app)
